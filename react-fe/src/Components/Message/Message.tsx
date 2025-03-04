a2575497b64448e7596e35c1c9066b5b6fb2d581
reesmonty <jacobreesmontgomery@gmail.com>
2025-03-03 20:05:49 -0500

    beautiful

--- react-fe/src/Components/Message/Message.tsx
+++ react-fe/src/Components/Message/Message.tsx
@@ -1,8 +1,10 @@
+import React, { useState } from "react";
 import Markdown from "react-markdown";
 import remarkGfm from "remark-gfm";
 import rehypeHighlight from "rehype-highlight";
 import styled from "styled-components";
-import { Database } from "react-feather";
+import { Database, XCircle } from "react-feather";
+import ReactModal from "react-modal";
 
 import {
     MessageProps,
@@ -55,10 +57,35 @@ const DatabaseIcon = styled(Database)`
     padding: 0;
 `;
 
+const customStyles = {
+    content: {
+        top: "50%",
+        left: "50%",
+        right: "auto",
+        bottom: "auto",
+        marginRight: "-50%",
+        transform: "translate(-50%, -50%)",
+        maxWidth: "50%",
+        fontFamily: "monospace",
+        borderRadius: "0.75rem",
+        boxShadow: "0 0.125rem 0.25rem rgba(0, 0, 0, 0.1)",
+    },
+};
+
 export default function Message({ openai_message, props }: MessageType) {
     // NOTE: AI responses are markdown-formatted, so we must render them as such.
     const { role, content } = openai_message as OpenAIMessage;
     const { executed_query } = (props as MessageProps) || {};
+    const [modalIsOpen, setIsOpen] = useState(false);
+
+    function openModal() {
+        setIsOpen(true);
+    }
+
+    function closeModal() {
+        setIsOpen(false);
+    }
+
     return (
         <MessageWrapper $role={role}>
             <MessageBubble $role={role}>
@@ -75,9 +102,28 @@ export default function Message({ openai_message, props }: MessageType) {
             </MessageBubble>
             {executed_query && (
                 <WidgetsContainer>
-                    <DatabaseButton onClick={() => console.log(executed_query)}>
+                    <DatabaseButton onClick={openModal}>
                         <DatabaseIcon />
                     </DatabaseButton>
+                    <ReactModal
+                        isOpen={modalIsOpen}
+                        onRequestClose={closeModal}
+                        style={customStyles}
+                        contentLabel="Executed Query"
+                    >
+                        <XCircle
+                            size={20}
+                            onClick={closeModal}
+                            style={{
+                                cursor: "pointer",
+                                position: "absolute",
+                                top: "0.35rem",
+                                right: "0.35rem",
+                            }}
+                        />
+                        <h2>Here was the executed query:</h2>
+                        <div>{executed_query}</div>
+                    </ReactModal>
                 </WidgetsContainer>
             )}
         </MessageWrapper>
